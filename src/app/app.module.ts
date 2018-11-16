9eeac9890ece9448717780f684b64a8d2e4684c1
Napster1010 <aditya.ndps@gmail.com>
2018-11-17 03:32:45 +0530

    Created the login form and performed basic validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AlertModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    AlertModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(){
+    this.submitted = true;
+  }
+}
